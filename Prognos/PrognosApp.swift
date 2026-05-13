bfab036ecac83aa0c1acf9f9adcf4e27949b3d40
MrSampaio <jusampa2@gmail.com>
2026-05-13 17:05:54 -0300

    Initial Commit

---
+++ Prognos/PrognosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrognosApp.swift
+//  Prognos
+//
+//  Created by Julio Sampaio on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PrognosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
