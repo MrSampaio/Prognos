7352ee1c01bedbebdeede6d173276349ae734e03
MrSampaio <jusampa2@gmail.com>
2026-05-14 14:21:37 -0300

    teste2

--- Prognos/PrognosApp.swift
+++ Prognos/PrognosApp.swift
@@ -10,7 +10,11 @@ import SwiftUI
 @main
 struct PrognosApp: App {
     var body: some Scene {
+       
         WindowGroup {
+            HStack{
+                Text("salve leo")
+            }
             ContentView()
         }
     }
