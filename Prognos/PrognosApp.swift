cf2930d8dc3b511690658123cb600553b1018057
Leo-gsilva <leonardosilva200610@gmail.com>
2026-05-14 14:23:05 -0300

    não

--- Prognos/PrognosApp.swift
+++ Prognos/PrognosApp.swift
@@ -14,6 +14,7 @@ struct PrognosApp: App {
         WindowGroup {
             HStack{
                 Text("salve leo")
+                Text("Não")
             }
             ContentView()
         }
